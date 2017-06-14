0401e9bd9ae9598061b804dc6d3a6c746a70e6ec
otherworldlyguardian <otherworldlyguardian@gmail.com>
2017-06-14 11:46:37 -0400

    tough one

--- index.js
+++ index.js
@@ -1,2 +1,36 @@
+function displayError () {
+  $('#errors').html("I'm sorry, there's been an error. Please try again.")
+}
+
+function searchRepositories () {
+  $.get(`https://api.github.com/search/repositories?q=${searchTerms.value}`, function (data) {
+    const repoList = '<ul>' + data.items.map(r => {
+      return (
+        `<li>
+           <h2>${r.name}<a href="${r.html_url}">Check out this Repo</a></h2>
+           <p><a href="${r.owner.url}">${r.owner.login}</a>
+           <p>${r.description}</p>
+           <a href="#" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a>
+         </li>`
+      )
+    }).join('') + '</ul>'
+    document.getElementById("results").innerHTML = repoList
+  }).fail(displayError())
+  searchTerms.value = ''
+}
+
+function showCommits (el) {
+  let n = el.dataset.repository
+  let o = el.dataset.owner
+  $.get(`https://api.github.com/repos/${o}/${n}/commits`, function (data) {
+    const commitsList = 'ul' + data.map(c => {
+      return (
+        `<li>${c.commit.tree.sha}</li>`
+      )
+    })
+    document.getElementById('details').innerHTML = commitsList
+  }).fail(displayError())
+}
+
 $(document).ready(function (){
 });
